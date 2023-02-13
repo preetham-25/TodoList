32f006b377fa5aab5e7f8d41aa9f295b023626f8
preetham-25 <preethamsingabattu25@gmail.com>
2023-02-13 21:33:57 +0530

    added all code files

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET-KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
+from routes import *
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    
